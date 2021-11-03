a9e0399cff3b128b336360ae50277e1f43e6ab79
ElHefe3 <koosob@gmail.com>
2021-11-03 17:21:46 +0200

    Added data context
    AppConfigguration.cs
    DatabaseContext.cs
    DatabaseContextFactory.cs

--- DAL/DataContext/AppConfiguration.cs
+++ DAL/DataContext/AppConfiguration.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace DAL.DataContext
 {
-    internal class AppConfiguration
+    public class AppConfiguration
     {
+        public AppConfiguration()
+        {
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            configurationBuilder.AddJsonFile(path, false);
+            IConfigurationRoot root = configurationBuilder.Build();
+            IConfigurationSection appSettings = root.GetSection("ConnectionStrings:DefaultConnection");
+            SqlConnectionString = appSettings.Value;
+        }
+
+        public string SqlConnectionString { get; set; }
     }
 }
---
+++ DAL/DataContext/DatabaseContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.DataContext
+{
+    public class DatabaseContext : DbContext
+    {
+        public class OptionsBuild
+        {
+            public OptionsBuild()
+            {
+                settings = new AppConfiguration();
+                OptionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
+                OptionsBuilder.UseSqlServer(settings.SqlConnectionString);
+                DatabaseOptions = OptionsBuilder.Options;
+            }
+
+            public DbContextOptionsBuilder<DatabaseContext> OptionsBuilder { get; set; }
+            public DbContextOptions<DatabaseContext> DatabaseOptions { get; set; }
+            private AppConfiguration settings { get; set; }
+        }
+
+        public static OptionsBuild ops = new OptionsBuild();
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options): base(options) { }
+    }
+
+    
+}
