95b219c5c72b4e7a2241a8432adb6930cf4dd487
ElHefe3 <71734422+ElHefe3@users.noreply.github.com>
2021-11-18 12:36:24 +0200

    login component

--- Web_App/galleryWebApp/src/app/Login/login.component.ts
+++ Web_App/galleryWebApp/src/app/Login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ApplicationRef, Component, OnInit } from '@angular/core';
+import { UserResult } from '../models/user-result.model';
+import { User } from '../models/user.model';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
-  constructor() { }
+  currentUser: User = new User();
+  constructor(private userService:UserService ) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
   }
 
+  async SubmitUser() {
+    let result = new UserResult();
+    await this.userService
+      .UserLogin(this.currentUser)
+      .then((data) => {
+        result.success = data.success;
+        result.userMessage = data.userMessage;
+        if(result.success) {
+          alert('Login Sucessful!')
+        } else {
+          alert(result.userMessage);
+        }
+        this.currentUser = new User();
+      })
+  }
 }
--- Web_App/galleryWebApp/src/app/models/user-result.model.ts
+++ Web_App/galleryWebApp/src/app/models/user-result.model.ts
@@ -1,2 +1,7 @@
-export class UserResult {
+import { Iresult } from "../interfaces/iresult";
+import { User } from "./user.model";
+export class UserResult implements Iresult {
+    success:boolean=false;
+    userMessage:string="";
+    result_set:User[]=[];
 }
